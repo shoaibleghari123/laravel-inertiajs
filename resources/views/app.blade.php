5b40aeac9355e6ba3322e8a8c6e79b1c69de3213
muhammad_shoaib <muhammad_shoaib@pitb.gov.pk>
2025-01-19 21:14:36 +0500

    dynamic imports

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <title>Laravel Inertia Js</title>
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
     <link href="https://unpkg.com/tailwindcss@2/dist/tailwind.min.css" rel="stylesheet">
-
-
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
+    <script src="{{ mix('/js/manifest.js') }}" defer></script>
+    <script src="{{ mix('/js/vendor.js') }}" defer></script>
     <script src="{{ mix('/js/app.js') }}" defer></script>
-    @inertiaHead
+{{--    @inertiaHead--}}
 </head>
 <body>
     @inertia
