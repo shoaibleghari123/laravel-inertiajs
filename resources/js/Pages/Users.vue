ab35b6a6c8a223426a034a010be12ff7bb44316b
muhammad_shoaib <muhammad_shoaib@pitb.gov.pk>
2025-01-20 22:45:26 +0500

    Remove storage folder from tracking

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -4,20 +4,13 @@
 
     <h1 class="text-3xl">Users</h1>
 
-    <div style="margin-top: 400px">
-        <p>The current time is {{ times }}</p>
-
-    </div>
-
-    <div>
-        <Link href="/users" class="text-blue-500" preserve-scroll>
-            Refresh
-        </Link>
-    </div>
+    <ul>
+        <li v-for="user in users" :key="user.id" v-text="user.name"></li>
+    </ul>
 </template>
 
 <script setup>
-    defineProps({ times: String });
+    defineProps({ users: Array });
 </script>
 
 
